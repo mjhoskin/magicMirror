d4dd933c3d282234c0853dbc4e1e2b2de8bedc32
Mike Hoskin <46720128+mjhoskin@users.noreply.github.com>
2025-01-31 16:29:37 +0000

    Update config.js

--- config.js
+++ config.js
@@ -38,111 +38,43 @@ let config = {
         // true, force serveronly mode, because you want to.. no UI on this device
 	
 modules: [
-    {
-		module: "alert",
-	},
-	{
-		module: "clock",
-		position: "top_right"
-	},
-	{
-		module: 'MMM-CalendarExt2',
-		config: {
-			roatateInterval: 0,
-			updateInterval: 1000*60*15,
-			calendars : [
-			{
-				url: "MyFamilyCalendarURL",
-			},
-			{
-				url: "https://www.google.com/calendar/ical/en.usa%23holiday@group.v.calendar.google.com/public/basic.ics",
-			},
-			],
-		views: [
-			{
-				name: "view1",
-				mode: "month",
-				maxItems: 1000,
-				hideOverflow: false,
-				slotMaxHeight: 100px, //100px works great for months that show 5 weeks on the screen - use 90px when a month flows over into a 6th week
-				monthFormat: "MMMM YYYY",
-				timeFormat: "h:mm A",
-				position: "top_left",
-			},
-			],
-		scenes: [
-			{
-				name: "DEFAULT",
-			},
-			],
+	    	{
+			module: "alert",
 		},
-	},
-	{
-                module: "weather",
-                position: "top_right",
-                header: "Current Weather for",
-                config: {
-                        weatherProvider: "weathergov",
-                        showHumidity: true,
-                        apiBase: 'https://api.weather.gov/points/',
-                        type: "current",
-                        lat: 43.0077,
-                        lon: -85.7777,
-                }
-        },
-        {
-                module: "weather",
-                position: "top_right",
-                header: "Weather Forecast for",
-                config: {
-                        weatherProvider: "weathergov",
-                        colored: false,
-                        fade: false,
-                        ignoreToday: true,
- 			location: "Walker, MI",
-                        apiBase: 'https://api.weather.gov/points/',
-                        lat: 43.0077,
-                        lon: -85.7777,
-                        type: "forecast",
-                }
-        },
-    {
-        module: "newsfeed",
-        position: "bottom_bar",
-        config: {
-            feeds: [
-                {
-                    title: "New York Times",
-                    url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-                },
-                {
-                    title: "NPR",
-                    url: "https://feeds.npr.org/1001/rss.xml"
-                },
-                {
-                    title: "WOOD TV 8",
-                    url: "https://www.woodtv.com/feed/"
-                },
-            ],
-            showSourceTitle: true,
-            showPublishDate: true,
-            broadcastNewsFeeds: true,
-            broadcastNewsUpdates: true,
-            ignoreOldItems: true,
-            ignoreOlderThan: 604800000
-        }
-    },
-    {
-		module: 'MMM-MicrosoftToDo',
-		position: 'top_right',      // This can be any of the regions. Best results in left or right regions.
-		header: 'groceries', // This is optional
-		config: {
-			oauth2ClientSecret: 'YourSecretID',
-			oauth2RefreshToken: 'yourRefreshToken',
-			oauth2ClientId: 'yourClientID',
-			listName: 'groceries', // optional parameter: if not specified displays tasks from default "Tasks" list, if specified will look for a task list with the specified name (exact spelling), don't specify if you want to make use of the>      refreshSeconds: 60, // optional parameter: every how many seconds should the list be updated from the remote service, default value is 60
-		}
-	},
+		{
+			module: "clock",
+			position: "top_right"
+		},
+		{
+			module: 'MMM-CalendarExt2',
+			config: {
+				roatateInterval: 0,
+				updateInterval: 1000*60*15,
+				calendars : [
+						{
+							url: "MyFamilyCalendarURL",
+						},
+					],
+				views: [
+						{
+							name: "view1",
+							mode: "month",
+							locale: "en-gb",
+							maxItems: 1000,
+							hideOverflow: false,
+							slotMaxHeight: "auto", // "100px",
+							monthFormat: "MMMM YYYY",
+							timeFormat: "h:mm A",
+							position: "top_left",
+						},
+					],
+				scenes: [
+						{
+							name: "DEFAULT",
+						},
+					],
+			},
+		},
         ]
 };
 
